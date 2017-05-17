62de1cb838080bbffcbe8bf8db07bada9bb26f28
mansour <almansour2345@gmail.com>
2017-05-17 11:04:56 +0300

    Update moagitapiwithtk2.py

--- moagitapiwithtk2.py
+++ moagitapiwithtk2.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import Tkinter
 from Tkconstants import *
+import re
 
 import sys
 import commands
@@ -36,21 +37,39 @@ p= r.text
 
 tfajr=p.find('fajr')
 fp0=p.find('am',tfajr)
-
-tl0=p[tfajr:fp0]
+jl0=p[tfajr:fp0]
+tj0=jl0[0:4]
+tj1=jl0[7:11]
+tl0=tj0 + '\t-_' + tj1+'_-'
+ 
+#tl0=p[tfajr:fp0]
 # print tl0
 
 tduher=p.find('dhuhr')
-fp1=p.find('am',tduher)
+fp1=p.find('pm',tduher)
 
 tl1=p[tduher:fp1]
+td0=tl1[0:5]
+td1=tl1[7:14]
+tl1=td0 + '\t-_' + td1+'_-'
+
+
 # print tl1
+
+
 
 tasr=p.find('asr')
 fp2=p.find('pm',tasr)
 
 tl2=p[tasr:fp2]
+tr0=tl2[0:4]
+tr1=tl2[6:11]
+tl2=tr0 + '\t-_' + tr1+'_-'
+
+
 # print tl2
+
+
 
 tmaghrib=p.find('maghrib')
 fp3=p.find('pm',tmaghrib)
@@ -66,33 +85,75 @@ tl4=p[tisha:fp4]
 
 
  
-#### starting the GUI "user interface" and inserting the times 
  
+ 
+ 
+ 
+ms = re.search('(\b)?(\d+):(\d+)',tl0)
+ms1 = re.search('(\b)?(\d+):(\d+)',tl1)
+ms2 = re.search('(\b)?(\d+):(\d+)',tl2)
+ms3 = re.search('(\b)?(\d+):(\d+)',tl3)
+ms4 = re.search('(\b)?(\d+):(\d+)',tl4)
 
+dr = ms.group()
+dr1 = ms1.group()
+dr2 = ms2.group()
+dr3 = ms3.group()
+dr4 = ms4.group()
+
+fdr="Fajer Prayer (" + dr + ")"
+dudr="Duhur Prayer (" + dr1 + ")"
+asrdr="Asur Prayer (" + dr2 + ")"
+mgdr="Magrib Prayer (" + dr3 + ")"
+esdr="Esha Prayer (" + dr4 + ")"
+#### starting the GUI "user interface" and inserting the times 
+### a def to capture keyboard events 
+
+
+def key(event):
+	frame.focus_set()
+	tk.destroy() , repr(event.char)
+ 
 tk.title("Moaget Al-Salat")
-tk.geometry("800x600")			
-frame = Tkinter.Frame(tk, relief=RIDGE, borderwidth=5,width=600, height=1000, bg="black")
-frame.config(bg="orange")
+tk.geometry("450x500")		
+
+## here execute the key event and exit the app
+	
+tk.bind("<Key>", key)
+
+
+frame = Tkinter.Frame(tk, relief=RIDGE, borderwidth=5,width=600, height=1000)#, bg="gray12"
+
+
+
+frame.config(bg="gray9")
+
+#frame.bind("<Button-1>", callback)
 frame.pack(fill=BOTH,expand=3)
+
 frame.pack_propagate(3)
-label = Tkinter.Label(frame, text="time now", font=("Helvetica", 21))
+
+
+
+label = Tkinter.Label(frame, text="Time Now", font=("Helvetica", 24),bg="gray38")
 label.pack(side=TOP,fill=X, expand=3,padx=5, pady=5)
-label7= Tkinter.Label(frame, text=a , font=("Helvetica", 18))
+label7= Tkinter.Label(frame, text=a , font=("Helvetica", 21),bg="gray35")
 label7.pack(side=TOP,fill=X, expand=5,padx=5, pady=5)
-label1 = Tkinter.Label(frame, text="time of prayers", font=("Helvetica", 21))
+label1 = Tkinter.Label(frame, text="Prayer\'s Times ", font=("Helvetica", 21),bg="gray78")
 label1.pack(side=TOP,fill=X, expand=3,padx=5, pady=5)
-label2= Tkinter.Label(frame, text=tl0, font=("Helvetica", 18))
+label2= Tkinter.Label(frame, text=fdr, font=("Helvetica", 21),bg="gray59")
 label2.pack(side=TOP,fill=X, expand=5,padx=5, pady=5)
-label3= Tkinter.Label(frame, text=tl1, font=("Helvetica", 18))
+label3= Tkinter.Label(frame, text=dudr, font=("Helvetica", 21),bg="gray45")
 label3.pack(side=TOP,fill=X, expand=5,padx=5, pady=5)
-label4= Tkinter.Label(frame, text=tl2, font=("Helvetica", 18))
+label4= Tkinter.Label(frame, text=asrdr, font=("Helvetica", 21),bg="gray43")
 label4.pack(side=TOP,fill=X, expand=5,padx=5, pady=5)
-label5= Tkinter.Label(frame, text=tl3, font=("Helvetica", 18))
+label5= Tkinter.Label(frame, text=mgdr, font=("Helvetica", 21),bg="gray39")
 label5.pack(side=TOP,fill=X, expand=5,padx=5, pady=5)
-label6= Tkinter.Label(frame, text=tl4, font=("Helvetica", 18))
+label6= Tkinter.Label(frame, text=esdr, font=("Helvetica", 21),bg="gray35")
 label6.pack(side=TOP,fill=X, expand=5,padx=5, pady=5)
 button = Tkinter.Button(frame,text="Exit",command=tk.destroy)
+#button.bind("<Key>", key)
+
 button.pack(side=RIGHT)
+
 tk.mainloop()
-
-
