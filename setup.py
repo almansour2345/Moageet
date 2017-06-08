07eac1d8407fc567319e4271a7955be3e79aba21
mansour <almansour2345@gmail.com>
2017-06-08 07:53:48 +0300

    Create setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
      name='moagit',
 
-     version='1.0.0',
+     version='7.0.0',
 
      description='Moagit al salah',
 
@@ -20,31 +20,8 @@ setup(
 
      packages=find_packages(exclude=['']),
 
-     install_requires=['requests','tk'],
-	 classifiers=[
-			# How mature is this project? Common values are
-			#   3 - Alpha
-			#   4 - Beta
-			#   5 - Production/Stable
-			'Development Status :: 3 - Alpha',
-
-			# Indicate who your project is intended for
-			'Intended Audience :: Developers',
-			'Topic :: Software Development :: Build Tools',
-
-			# Pick your license as you wish (should match "license" above)
-			'License :: OSI Approved :: MIT License',
-
-			# Specify the Python versions you support here. In particular, ensure
-			# that you indicate whether you support Python 2, Python 3 or both.
-			'Programming Language :: Python :: 2',
-			'Programming Language :: Python :: 2.7',
-			'Programming Language :: Python :: 3',
-			'Programming Language :: Python :: 3.3',
-			'Programming Language :: Python :: 3.4',
-			'Programming Language :: Python :: 3.5',
-		],
-		py_modules=['Tkinter','Tkconstants','sys','commands','time','datetime','requests'],
+     install_requires=['requests'],
+ 
 #     entry_points={
 #        'gui_scripts': [
 #
